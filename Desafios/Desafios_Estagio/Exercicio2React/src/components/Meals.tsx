b638470b47807537096569f81c33ec880690229f
Martim Alves Marques <marques.martim3@gmail.com>
2022-07-04 16:45:05 +0100

    Restaurant_ExercicioReact

---
+++ Desafios/Desafios_Estagio/Exercicio2React/src/components/Meals.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { meal } from "../App";
+import "./Meals.css";
+
+const Meal = ({
+  name,
+  meals,
+  onChange,
+}: {
+  name: string;
+  meals: meal[];
+  onChange: (newPrice: number) => void;
+}) => {
+  const handler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      onChange(+e.target.value);
+    } else {
+      onChange(+e.target.value * -1);
+    }
+  }; // 100
+
+  return (
+    <>
+      <div className="expense-item">
+        <br></br>
+        {name}
+        <br></br>
+      </div>
+      <div className="expense-item__description">
+        {meals.map((meal) => {
+          return (
+            <div>
+              <br></br>
+              <img
+                src={meal.strMealThumb}
+                alt={meal.strMeal}
+                className="expense-image"
+              />
+              {meal.strMeal} {meal.price}${" "}
+              <input
+                type="checkBox"
+                value={meal.price}
+                onChange={handler}
+              ></input>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Meal;
